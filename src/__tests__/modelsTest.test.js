071acd70c4a62d91be9fb5aa6002d718d4f84f1a
Aleksandr Tereshchenko <terap98@icloud.com>
2017-10-01 09:41:41 +0300

    Fixed existing tests, added .travis.yml file

--- src/__tests__/modelsTest.test.js
+++ src/__tests__/modelsTest.test.js
@@ -4,17 +4,26 @@
 import dotenv from 'dotenv'
 // import chai from 'chai'
 import logger from 'winston'
+dotenv.config()
 
 import { User } from '~/models/'
 import database from '~/models/MongoDatabase'
 
-dotenv.config()
 
 // let expect = chai.expect
 
 describe('User model:', async function () {
   beforeAll(async function () {
-    User.collection.drop()
+    await User.collection.drop()
+    try {
+      await User.find()
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  afterAll(async function () {
+    await User.collection.drop()
   })
   // const User = models.User
   // logger.info(`Testing user model. Database ${User.getDb().url} and collection ${User.getCollectionName()}`)
@@ -75,8 +84,9 @@ describe('User model:', async function () {
     let updatedCount = await User.update(
       {email: 'example1@example.com'},
       {firstName: 'Penny'}
-    ).count()
-    expect(updatedCount).toEqual(1)
+    )
+
+    expect(updatedCount.ok).toEqual(1)
 
     let mrNode = await User.findOne({email: 'example2@example.com'})
     expect(mrNode).not.toEqual(null)
@@ -89,28 +99,27 @@ describe('User model:', async function () {
     }
 
     let penny = await User.findOne({firstName: 'Penny'})
+    console.log(penny, 'here should be a Peenyy')
     expect(penny.firstName).toEqual('Penny')
   })
   it('finds many users from the database', async function () {
-    let usersWithLasName = await User.find({lastName: { '$exists': true }})
-    expect(usersWithLasName.toArray()).to.have.length(4)
+    let usersWithLastName = await User.find({lastName: { '$exists': true }})
+    expect(usersWithLastName.length).toEqual(4)
   })
   it('cleans up all the users in the database', async function () {
     let allUsers = await User.find()
-    expect(allUsers.toArray()).to.have.length(4)
-    await User.delete({ email: 'example@example.com' }) // delete one using instance method
+    expect(allUsers.length).toEqual(4)
+    await User.remove({ email: 'example@example.com' }) // delete one using instance method
     let count = await User.count()
     expect(count).toEqual(3)
-    let deletedCount = await User.delete({email: 'example1@example.com'})// delete a user using static method
-    expect(deletedCount).toEqual(1)
+    let deleteResult = await User.remove({email: 'example1@example.com'})// delete a user using static method
+
+    expect(deleteResult.result.ok).toEqual(1)
     count = await User.count()
     expect(count).toEqual(2)
-    await User.delete() // delete the rest
+    await User.remove() // delete the rest
     count = await User.count()
     expect(count).toEqual(0)
-    // in general you should avoid using anything database-specific outside of the models folder
-    // but we are doing it for testing here, so this can be an exception
-    await User.collection.drop()
   })
 })
 /* Test associations between user and comment
